6a5adeaa868fffe9bafc263c65f8a76c03656a3d
reglid <dominik@regli.info>
2019-11-02 18:46:51 +0100

    Added actual quests

--- src/app/tresor-dialog/tresor-dialog.component.ts
+++ src/app/tresor-dialog/tresor-dialog.component.ts
@@ -20,20 +20,40 @@ export class TresorDialogComponent implements OnInit {
     this.stages = [
       {
         id: 1,
-        quest: "Wie lautet die Lösung des ersten Rätsels?",
-        solution: "1234"
+        quest: "Rechnet: 1234 + 88 - 456 + 10",
+        solution: "876"
       }, {
         id: 2,
-        quest: "Das nächste Rätsel ist im Garten. Wie lautet die Lösung?",
-        solution: "5678"
+        quest: "Das nächste Rätsel findet ihr hinter dem Wandteppich. Wie lautet die Lösung?",
+        solution: "DBBCBDACCCBA"
       }, {
         id: 3,
-        quest: "Was ergibt 1000 + 200 + 30 + 4?",
-        solution: "5678"
+        quest: "Aurelius stellt euch eine Aufgabe. Wenn ihr sie löst, verrät er euch die Lösung.",
+        solution: "TigiBusi"
+      }, {
+        id: 4,
+        quest: "Das nächste Rätsel findet ihr in der Garage. Wie lautet die Lösung?",
+        solution: "9"
+      }, {
+        id: 5,
+        quest: "Das nächste Rätsel findet ihr in Eugenias Zimmer. Wer ist es?",
+        solution: "Mathilda"
+      }, {
+        id: 6,
+        quest: "Das nächste Rätsel findet ihr beim Schlagzeug. Schreibt die Rechnung ins Lösungsfeld",
+        solution: "2+1=3"
+      }, {
+        id: 7,
+        quest: "Philomena stellt euch eine Aufgabe im Atelier. Wenn ihr alles richtig löst, verrät er euch das Lösungswort.",
+        solution: "Hamster"
+      }, {
+        id: 8,
+        quest: "Bald habt ihr es geschafft. Das letzte Rätsel ist in Zohars Zimmer. Viel Glück!",
+        solution: "Marco"
       }
     ];
 
-    this.finalMessage = "Geschafft! Ihr könnt das Schloss mit der Kombination '971' öffnen. Viel Spass!";
+    this.finalMessage = "Geschafft! Ihr könnt das Schloss mit der Kombination '791' öffnen. Viel Spass!";
 
     this.currentStage = this.stages[this.currentStageIdx];
   }
